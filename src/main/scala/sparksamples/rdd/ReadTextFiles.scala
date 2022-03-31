c42d0b59471626045f78395b4cd0bf7fcbf47af8
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2022-03-31 21:34:00 +0530

    Scala samples code added

--- src/main/scala/sparksamples/rdd/ReadTextFiles.scala
+++ src/main/scala/sparksamples/rdd/ReadTextFiles.scala
@@ -7,44 +7,23 @@ object ReadTextFiles extends App {
 
   val spark:SparkSession = SparkSession.builder()
     .master("local[1]")
-    .appName("SparkByExamples.com")
+    .appName("ReadTextFiles")
     .getOrCreate()
-
   spark.sparkContext.setLogLevel("ERROR")
-
   println("##spark read text files from a directory into RDD")
-  val rddFromFile = spark.sparkContext.textFile("src/main/resources/csv/text01.txt")
-  println(rddFromFile.getClass)
+  val rddFromFile = spark.sparkContext.textFile("src/main/resources/csv/data.csv")
+  val rdd = rddFromFile.map(f=>{
+    f.split(",")
+  })
+  rdd.foreach(f=>{
+    println("Col1:"+f(0)+",Col2:"+f(1))
+  })
 
-  println("##Get data Using collect")
+  /*
   rddFromFile.collect().foreach(f=>{
     println(f)
   })
+  */
 
-  println("##read multiple text files into a RDD")
-  val rdd4 = spark.sparkContext.textFile("src/main/resources/csv/text01.txt," +
-    "src/main/resources/csv/text02.txt")
-  rdd4.foreach(f=>{
-    println(f)
-  })
-
-  println("##read text files base on wildcard character")
-  val rdd3 = spark.sparkContext.textFile("src/main/resources/csv/text*.txt")
-  rdd3.foreach(f=>{
-    println(f)
-  })
-
-  println("##read all text files from a directory to single RDD")
-  val rdd2 = spark.sparkContext.textFile("src/main/resources/csv/*")
-  rdd2.foreach(f=>{
-    println(f)
-  })
-
-  println("##read whole text files")
-  val rddWhole:RDD[(String,String)] = spark.sparkContext.wholeTextFiles("src/main/resources/csv/text01.txt")
-  println(rddWhole.getClass)
-  rddWhole.foreach(f=>{
-    println(f._1+"=>"+f._2)
-  })
 }
 
